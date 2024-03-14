71d07c1d2099d964ac4a9fa9e30b653e7a251162
Corin Wagen <corin.wagen@gmail.com>
2024-03-14 16:09:36 -0400

    updates

--- rowan/client.py
+++ rowan/client.py
@@ -24,17 +24,21 @@ class Client:
     def __post_init__(self):
         self.headers = {"X-API-Key": rowan.utils.get_api_key()}
 
+        if not self.blocking and self.delete_when_finished:
+            print("Warning: ``delete_when_finished`` has no effect when ``blocking`` is False. To delete calculations, call ``Client.delete()`` manually.")
+
     def compute(
         self,
         type: Optional[str] = "calculation",
         input_mol: Optional[cctk.Molecule] = None,
         input_smiles: Optional[str] = None,
         name: Optional[str] = None,
-        folder_id: Optional[str] = None,
+        folder_uuid: Optional[str] = None,
+        engine: str = "peregrine",
         **options,
     ) -> dict | str:
         if (input_mol is None) == (input_smiles is None):
-            raise ValueError("Must specify exactly one of ``input_smiles`` and ``input_molecule``!")
+            raise ValueError("Must specify exactly one of ``input_smiles`` and ``input_mol``!")
 
         if input_mol is None:
             input_mol = cctk.Molecule.new_from_smiles(input_smiles)
@@ -44,14 +48,14 @@ class Client:
         with httpx.Client() as client:
             if type == "calculation":
                 settings = stjames.Settings(**options)
-                calc = stjames.Calculation(molecules=[molecule], name=name, settings=settings)
+                calc = stjames.Calculation(molecules=[molecule], name=name, engine=engine, settings=settings)
 
                 response = client.post(
                     f"{API_URL}/calculation",
                     headers=self.headers,
                     json={
                         "json_data": calc.model_dump(mode="json"),
-                        "folder_id": folder_id,
+                        "folder_uuid": folder_uuid,
                     },
                 )
 
@@ -62,38 +66,38 @@ class Client:
                     json={
                         "initial_molecule": molecule.model_dump(mode="json"),
                         "name": name,
-                        "folder_id": folder_id,
+                        "folder_uuid": folder_uuid,
                         "workflow_data": options,
                     },
                 )
 
             response.raise_for_status()
             response_dict = response.json()
-            calc_id = response_dict["uuid"]
+            calc_uuid = response_dict["uuid"]
 
         if self.blocking:
-            while not self.is_finished(calc_id, type):
+            while not self.is_finished(calc_uuid, type):
                 time.sleep(self.ping_interval)
-            result = self.get(calc_id, type)
+            result = self.get(calc_uuid, type)
 
             if self.delete_when_finished:
-                self.delete(calc_id, type)
+                self.delete(calc_uuid, type)
 
             return result
 
         else:
-            return calc_id
+            return calc_uuid
 
-    def is_finished(self, calc_id: str, type: str = "calculation") -> bool:
+    def is_finished(self, calc_uuid: str, type: str = "calculation") -> bool:
         with httpx.Client() as client:
             if type == "calculation":
-                response = client.get(f"{API_URL}/calculation/{calc_id}", headers=self.headers)
+                response = client.get(f"{API_URL}/calculation/{calc_uuid}", headers=self.headers)
                 response.raise_for_status()
                 response_dict = response.json()
                 status = response_dict["status"]
 
             elif type == "pka":
-                response = client.get(f"{API_URL}/pka/{calc_id}", headers=self.headers)
+                response = client.get(f"{API_URL}/pka/{calc_uuid}", headers=self.headers)
                 response.raise_for_status()
                 response_dict = response.json()
                 status = response_dict["object_status"]
@@ -103,16 +107,16 @@ class Client:
 
         return status in [2, 3, 4]
 
-    def get(self, calc_id: str, type: str = "calculation") -> dict:
+    def get(self, calc_uuid: str, type: str = "calculation") -> dict:
         with httpx.Client() as client:
             if type == "calculation":
-                response = client.get(f"{API_URL}/calculation/{calc_id}/stjames", headers=self.headers)
+                response = client.get(f"{API_URL}/calculation/{calc_uuid}/stjames", headers=self.headers)
                 response.raise_for_status()
                 response_dict = response.json()
                 return response_dict
 
             elif type == "pka":
-                response = client.get(f"{API_URL}/pka/{calc_id}", headers=self.headers)
+                response = client.get(f"{API_URL}/pka/{calc_uuid}", headers=self.headers)
                 response.raise_for_status()
                 response_dict = response.json()
                 return response_dict["object_data"]
@@ -120,30 +124,28 @@ class Client:
             else:
                 raise ValueError(f"Unknown type ``{type}``!")
 
-    def stop(self, calc_id: str, type: str = "calculation") -> None:
+    def stop(self, calc_uuid: str, type: str = "calculation") -> None:
         with httpx.Client() as client:
             if type == "calculation":
-                response = client.post(f"{API_URL}/calculation/{calc_id}/stop", headers=self.headers)
+                response = client.post(f"{API_URL}/calculation/{calc_uuid}/stop", headers=self.headers)
                 response.raise_for_status()
 
             elif type == "pka":
-                response = client.post(f"{API_URL}/pka/{calc_id}/stop", headers=self.headers)
+                response = client.post(f"{API_URL}/pka/{calc_uuid}/stop", headers=self.headers)
                 response.raise_for_status()
 
             else:
                 raise ValueError(f"Unknown type ``{type}``!")
 
-
-    def delete(self, calc_id: str, type: str = "calculation") -> None:
+    def delete(self, calc_uuid: str, type: str = "calculation") -> None:
         with httpx.Client() as client:
             if type == "calculation":
-                response = client.delete(f"{API_URL}/calculation/{calc_id}", headers=self.headers)
+                response = client.delete(f"{API_URL}/calculation/{calc_uuid}", headers=self.headers)
                 response.raise_for_status()
 
             elif type == "pka":
-                response = client.delete(f"{API_URL}/folder/{calc_id}", headers=self.headers)
+                response = client.delete(f"{API_URL}/folder/{calc_uuid}", headers=self.headers)
                 response.raise_for_status()
 
             else:
                 raise ValueError(f"Unknown type ``{type}``!")
-
