685a1ed3cb3798615c460541a1e12fcb0d92b82b
Corin Wagen <corin.wagen@gmail.com>
2025-01-09 17:28:10 -0500

    add charge workflow for lewis

--- rowan/rowan_rdkit/__init__.py
+++ rowan/rowan_rdkit/__init__.py
@@ -1,5 +1,5 @@
 from .chem_utils import (run_pka, run_tautomers, run_conformers, run_energy, 
-run_optimize, run_batch_pka, run_batch_tautomers, run_batch_energy, run_batch_optimize, run_batch_conformers)
+run_optimize, batch_pka, batch_tautomers, batch_energy, batch_optimize, batch_conformers, run_charges, batch_charges)
 
 __all__ = ["run_pka", "run_tautomers", "run_energy", "run_conformers", "run_optimize", 
-           "run_batch_pka", "run_batch_tautomers", "run_batch_energy", "run_batch_optimize",  "run_batch_conformers"]
+           "batch_pka", "batch_tautomers", "batch_energy", "batch_optimize",  "batch_conformers", "run_charges", "batch_charges"]
