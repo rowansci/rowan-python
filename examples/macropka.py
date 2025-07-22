063e3206830c142520a052dad14b0d23f7077e6d
Jonathon Vandezande <jevandezande@gmail.com>
2025-07-22 14:41:56 -0400

    Adds better type checking

--- examples/macropka.py
+++ examples/macropka.py
@@ -1,13 +1,12 @@
-import stjames
-
 import rowan
 
 # Set ROWAN_API_KEY environment variable to your API key or set rowan.api_key directly
 # rowan.api_key = "rowan-sk..."
 
 result = rowan.submit_macropka_workflow(
-    initial_smiles="C1CCOC(=O)C1=C[C@@H](OC(CC)CC)[C@H](NC(C)=O)[C@@H]([NH3+])C1CCC1",  # oseltamivir
-    name="oseltamivir macropka",
+    # oseltamivir
+    initial_smiles="C1CCOC(=O)C1=C[C@@H](OC(CC)CC)[C@H](NC(C)=O)[C@@H]([NH3+])C1CCC1",
+    name="Oseltamivir macropka",
 )
 
 
